4e37552a4d9df0c34c47530921741c7ccea8b13a
Ashrafujjaman Rafi <aashrafrafi16@gmail.com>
2026-09-03 23:16:05 +0600

    donee

--- script.js
+++ script.js
@@ -13,27 +13,36 @@ submit.addEventListener("click",function(){
      <button class="delete">Delete</button>
      <button class="complete">Complete</button>
      </div>`
-     
  }
  taskInput.value="";
+ localStorage.setItem("tasks",task)
+ 
 }
 )
+ const savedTask=localStorage.getItem("tasks")
+ if(savedTask!=""){
+         taskList.innerHTML+=`
+     <div class="task">
+     <span>${savedTask}</span>
+     <button class="delete">Delete</button>
+     <button class="complete">Complete</button>
+     </div>`
+    }
 taskList.addEventListener("click", function(event) {
     if (event.target.classList.contains("delete")) {
         event.target.parentElement.remove();
     }
     if (event.target.classList.contains("complete")) {
         event.target.parentElement.classList.toggle("completed");  
-    }
-    if(event.target.innerHTML==="Complete"){
+        if(event.target.innerHTML==="Complete"){
         event.target.innerHTML="Completed"
     }
     else{
         event.target.innerHTML="Complete"
     }
+    }
     });
-    
-    taskInput.addEventListener("keypress",function(event){
+    taskInput.addEventListener("keydown",function(event){
         if(event.key==="Enter"){
             submit.click();
         }
