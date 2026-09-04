59f42092c31936d0940b61fdcff50072dd44eb27
Ashrafujjaman Rafi <aashrafrafi16@gmail.com>
2026-09-04 22:13:58 +0600

    yesssss

--- script.js
+++ script.js
@@ -1,7 +1,13 @@
 const taskInput=document.getElementById("taskInput")
 const submit=document.getElementById("submit")
 const taskList=document.getElementById("taskList")
+
 const taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+
+const totalTask=document.getElementById("total")
+ const compTask=document.getElementById("comptask")
+ const incompTask=document.getElementById("incomptask")
+
 function show(){
     taskList.innerHTML=""
     taskArray.forEach(task => {
@@ -15,6 +21,8 @@ function show(){
      </div>`
     });
 }
+
+
 submit.addEventListener("click",function(){
  const task=taskInput.value;
  if(task===""){
@@ -28,16 +36,22 @@ submit.addEventListener("click",function(){
  }
  taskInput.value="";
  localStorage.setItem("tasks",JSON.stringify(taskArray))
+ updateProgress()
  show()
+
 }
 ) 
+
 show()
+
 taskList.addEventListener("click", function(event) {
     if (event.target.classList.contains("delete")) {
         const index=taskArray.findIndex(task=>task.text===event.target.parentElement.querySelector("span").innerHTML)
         event.target.parentElement.remove();
         taskArray.splice(index,1)
         localStorage.setItem("tasks", JSON.stringify(taskArray));
+    
+        updateProgress()
     }
     if (event.target.classList.contains("complete")) {
         event.target.parentElement.classList.toggle("completed");  
@@ -46,18 +60,34 @@ taskList.addEventListener("click", function(event) {
         event.target.innerHTML="Completed"
         taskArray[index].completed=!taskArray[index].completed
         localStorage.setItem("tasks", JSON.stringify(taskArray));
+         updateProgress()
     }
     else{
         event.target.innerHTML="Complete"
         taskArray[index].completed=!taskArray[index].completed
         localStorage.setItem("tasks", JSON.stringify(taskArray));
+        updateProgress()
     }
     }
     });
+
 taskInput.addEventListener("keydown",function(event){
     if(event.key==="Enter"){
          submit.click();
     }
-    })
+   })
 
+ function updateProgress(){
+totalTask.innerHTML=taskArray.length
+let compTaskk=0;
+taskArray.forEach(element => {
+    if(element.completed){
+        compTaskk++;
+    }
+});
+compTask.innerHTML=compTaskk
+totalTask.innerHTML=taskArray.length
+incompTask.innerHTML=taskArray.length - compTaskk
+}
+updateProgress()
     
