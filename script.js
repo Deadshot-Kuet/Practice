a0296e00bdf34d16bdf89f85e5966ade2d47cc3f
Ashrafujjaman Rafi <aashrafrafi16@gmail.com>
2026-09-04 12:34:20 +0600

    donee

--- script.js
+++ script.js
@@ -1,6 +1,18 @@
 const taskInput=document.getElementById("taskInput")
 const submit=document.getElementById("submit")
 const taskList=document.getElementById("taskList")
+const taskArray = JSON.parse(localStorage.getItem("tasks")) || [];
+function show(){
+    taskList.innerHTML=""
+    taskArray.forEach(task => {
+         taskList.innerHTML+=`
+     <div class="task">
+     <span>${task}</span>
+     <button class="delete">Delete</button>
+     <button class="complete">Complete</button>
+     </div>`
+    });
+}
 submit.addEventListener("click",function(){
  const task=taskInput.value;
  if(task===""){
@@ -13,24 +25,19 @@ submit.addEventListener("click",function(){
      <button class="delete">Delete</button>
      <button class="complete">Complete</button>
      </div>`
+     taskArray.push(task)
  }
  taskInput.value="";
- localStorage.setItem("tasks",task)
- 
+ localStorage.setItem("tasks",JSON.stringify(taskArray))
 }
-)
- const savedTask=localStorage.getItem("tasks")
- if(savedTask!=""){
-         taskList.innerHTML+=`
-     <div class="task">
-     <span>${savedTask}</span>
-     <button class="delete">Delete</button>
-     <button class="complete">Complete</button>
-     </div>`
-    }
+) 
+show()
 taskList.addEventListener("click", function(event) {
     if (event.target.classList.contains("delete")) {
+        const index=taskArray.indexOf(event.target.parentElement.querySelector("span").innerHTML)
         event.target.parentElement.remove();
+        taskArray.splice(index,1)
+        localStorage.setItem("tasks", JSON.stringify(taskArray));
     }
     if (event.target.classList.contains("complete")) {
         event.target.parentElement.classList.toggle("completed");  
@@ -47,3 +54,4 @@ taskList.addEventListener("click", function(event) {
             submit.click();
         }
     })
+    
